880e404d7c3a84ad1c7188303f171ed16eabd9d0
weru <onewesh@gmail.com>
2020-03-04 15:25:49 +0300

    save values in user defaults

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -7,32 +7,14 @@
 //
 
 import SwiftUI
- 
 
 struct ContentView: View {
-    @State private var numbers = [Int]()
-    @State private var currentNumber = 1
+    @State private var tapCount = UserDefaults.standard.integer(forKey: "Tap")
     var body: some View {
-        NavigationView {
-        VStack {
-            List {
-                ForEach(numbers, id: \.self) {
-                    Text("\($0)")
-                }
-            .onDelete(perform: removeRows)
-            }
-            
-            Button("Add Number") {
-                self.numbers.append(self.currentNumber)
-                self.currentNumber += 1
-            }
+        Button("Tap Count \(tapCount)") {
+            self.tapCount += 1
+            UserDefaults.standard.set(self.tapCount, forKey: "Tap")
         }
-        .navigationBarItems(leading: EditButton())
-        }
-    }
-    
-    func removeRows(at offsets: IndexSet) {
-        numbers.remove(atOffsets: offsets)
     }
 }
 
